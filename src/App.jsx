d975fe1cc94e6bf150770583e56e0cd8cde0bbbf
eddiemuhoro <eddiemuhoro@gmail.com>
2023-01-17 22:55:18 +0300

    added some files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import Hero from './assets/Hero/Hero'
 import Navbar from './assets/Navigation/Navbar'
 import Speciality from './assets/Speciality/Speciality'
+import WhyUs from './assets/whyUs/WhyUs'
 
 function App() {
-
+// patient is the user who books an appointment
+//doctor will accept and send join link to a zoom meeting
   return (
     <div className="App">   
-    
+    <Navbar />
+    <Hero />
      <Speciality/>
+     <WhyUs />
     </div>
   )
 }
---
+++ src/assets/whyUs/WhyUs.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../styles/whyUs.css'
+
+const WhyUs = () => {
+  return (
+    <div className='speciality-wrapper'>
+       <div className='title'>
+            <div>
+                <h1>Why Swasthu?</h1>
+                <p>Lorem ipsum de color iss slsysos kshss sshdusk hbnjfyo ckdhdoi</p>
+            </div>
+        </div>
+        <div className='why-us'>
+            <section className='image'>
+                <img src="https://firebasestorage.googleapis.com/v0/b/fir-api-7421d.appspot.com/o/test%2FFrame.png?alt=media&token=5724216e-36be-4d93-9317-0916ce275b63" alt="image" />
+            </section>
+            <section  className='why-text'>
+                <h2>Curated Doctors</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna gravida id lobortis eget tristique ultrices feugiat neque elit. Amet imperdiet donec cras habitant ultrices sed in mi scelerisque. Maecenas tempus amet aenean in eu ullamcorper viverra pulvinar. Nibh et elementum parturient viverra in. In morbi vehicula dignissim pharetra, tristique. 
+                </p>
+            </section>
+        </div>
+    </div>
+
+  )
+}
+
+export default WhyUs
